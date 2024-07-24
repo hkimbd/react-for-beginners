825af7842569dd49ff722ff88e261552e972829d
hkimbd <99864124+hkimbd@users.noreply.github.com>
2024-07-24 10:20:06 +0900

    minor change on styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -12,13 +12,12 @@ function Movie({id, coverImg, title, year, summary, genres}) {
             <Link to={`/movie/${id}`}>{title}</Link>
           </h2>
           <h3 className={styles.movie__year}>{year}</h3>
-          <p>{summary.length > 235 ? `${summary.slice(0,235)}...`: summary}</p>
           <ul className={styles.movie__genres}>
             {genres.map((g) => 
               <li key={g}>{g}</li>
             )}
           </ul>
-
+          <p>{summary.length > 235 ? `${summary.slice(0,235)}...`: summary}</p>
         </div>
       </div>
     ); 
